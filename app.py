379da7f2d1212570e79f0fed42c629d9ad4dc90b
MohamedBilelBesbes <bilel.besbes1@gmail.com>
2021-05-25 20:19:26 +0100

    prefinal version

---
+++ app.py
@@ -0,0 +1,232 @@
+from flask import Flask, render_template , request, jsonify, url_for , redirect , flash
+from datetime import datetime
+from pmdarima import auto_arima
+import pickle
+import pandas as pd
+import sys
+import os
+import glob
+import re
+import time
+import json
+
+import sys
+
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+from dateutil import relativedelta
+from flask import send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/uploading')
+def uploading():
+    return render_template('uploading.html')
+
+@app.route('/graphed')
+def graphed():
+    return render_template('graphed.html')
+
+@app.route('/graphed',methods=['POST'])
+def prediction():
+    x_test = [[x for x in request.form.values()]]
+    # ['shop_id', 'item_id' , 'Price' , 'starting date' , 'period']
+    x_test[0][0] = int(x_test[0][0])
+    x_test[0][1] = int(x_test[0][1])
+    x_test[0][2] = float(x_test[0][2])
+    numberofmonths = int(x_test[0][4])
+    startdate = datetime.strptime(x_test[0][3]+'-01', '%Y-%m-%d').date()
+    d = {
+    'shop_id': [x_test[0][0]],
+    'item_id': [x_test[0][1]],
+    'Price': [x_test[0][2]]}
+    try:
+        model = pickle.load(open('models/shop'+str(x_test[0][0])+'item'+str(x_test[0][1])+'.pkl', 'rb'))
+        df = pd.read_csv('datasets/shop'+str(x_test[0][0])+'item'+str(x_test[0][1])+'.csv', index_col=None)
+    except:
+        return render_template('index.html', 
+        prediction_text=
+  'item {} in shop {} does not exist in our data'.format(x_test[0][1],x_test[0][0]))
+    lastdateindf = df['month'].sort_values().iloc[-1]
+    lastdateindf = datetime.strptime(lastdateindf, '%Y-%m-%d').date()
+    df["month"] = pd.to_datetime(df["month"])
+    diffdate = (startdate.year - lastdateindf.year) * 12 + (startdate.month - lastdateindf.month)
+    if diffdate > 0:
+        forecaster = model.predict(n_periods=numberofmonths, return_conf_int=False)
+        forecaster = forecaster.astype('int64')
+        forecaster = list(forecaster)
+        for i in range(len(forecaster)):
+            if forecaster[i] < 0:
+                forecaster[i] = 0
+    elif((diffdate - 1)*(-1) > numberofmonths and diffdate != 0):
+        forecaster = df["quantity"].iloc[diffdate - 1:diffdate + numberofmonths - 1]
+        forecaster = forecaster.astype('int64')
+        forecaster = forecaster.tolist()
+    elif ((diffdate - 1)*(-1) == numberofmonths):
+        forecaster = df["quantity"].iloc[diffdate - 1:]
+        forecaster = forecaster.astype('int64')
+        forecaster = forecaster.tolist()
+    elif (-3 < diffdate < 1):
+        forecaster1 = df["quantity"].iloc[diffdate - 1:]
+        forecaster1 = forecaster1.astype('int64')
+        forecaster1 = forecaster1.tolist()
+        forecaster2 = model.predict(n_periods= numberofmonths + diffdate - 1, return_conf_int=False)
+        forecaster2 = forecaster2.astype('int64').tolist()
+        print(forecaster1)
+        print(forecaster2)
+        forecaster = forecaster1 + forecaster2
+    else:
+        return render_template('index.html',
+        prediction_text=
+  'item {} in shop {} does exist in our data but there seems to be a problem with the date that you putted'.format(x_test[0][1],x_test[0][0]))
+    dates = []
+    prices = []
+    for i in range(numberofmonths):
+        prices.append(x_test[0][2])
+        dates.append(startdate + relativedelta.relativedelta(months=i))
+    forecastdata = pd.DataFrame({'month':dates,'price':prices,'quantity':forecaster})
+    dfpred = df[["month","quantity"]]
+    dfpredict = forecastdata[["month","quantity"]]
+    dfpredict["quantity"] = dfpredict["quantity"].astype(float)
+    s1 = json.dumps(json.loads(dfpred.rename({'month': 'x', 'quantity': 'y'}, axis='columns').to_json(orient="records")))
+    # s2 = json.dumps(json.loads(dfpredict.rename({'month': 'x', 'quantity': 'y'}, axis='columns').to_json(orient="records")))
+    dfpredict = dfpredict.rename({'month': 'Date', 'quantity': 'item_cnt_month'}, axis='columns')
+    dfpred = dfpred.rename({'month': 'Date', 'quantity': 'predictions'}, axis='columns')
+    ss = dfpred.copy()
+    copyhist = df.copy().rename({'Price':'price'}, axis='columns')
+    copypred = forecastdata.copy()
+    copyhist["total sales"] = copyhist["quantity"] * copyhist["price"]
+    copypred["total sales"] = copypred["quantity"] * copypred["price"]
+    copyhist["month"] = pd.to_datetime(copyhist["month"]).dt.date
+    copyall = copyhist.append(copypred, ignore_index=True)
+    copyall["total sales"]=copyall["total sales"].apply(lambda x:round(x,3))
+    copyall["price"]=copyall["price"].apply(lambda x:round(x,3))
+    ss["shop_id"] = x_test[0][0]
+    ss["item_id"] = x_test[0][1]
+    result = dfpred.to_html()
+    return render_template('graphed.html',data=dfpredict.to_json(),data1=dfpred.to_json(),data2=ss.to_json(),ss=copyall.values)
+
+
+@app.route('/uploading', methods=['POST'])
+def process():
+    files = request.files["myfile"]
+    print(files.filename)
+    files.save(os.path.join('./uploads',files.filename))
+    try:
+        data = pd.read_csv(os.path.join('./uploads',files.filename),index_col=None)
+    except:
+            os.remove('./uploads/' + files.filename)
+            return render_template('uploading.html', 
+  textofdisplay=
+  'the uploaded file format is not .csv, please put the correct type of file',labeling = 'Red')
+    if list(data.columns)==['Date', 'shop_id', 'item_id', 'item_category', 'id_struct', 'Price', 'item_cnt_day']:
+        dictofdata = dataprocessing(data)
+        print("new data preprocessed")
+        storing(dictofdata)
+        print("new data stored")
+        os.remove('./uploads/' + files.filename)
+        return render_template('uploading.html', 
+  textofdisplay=
+  'Process successful! Would you like to add another dataset to enhance your models ?',labeling = 'Green')
+    else:
+        print('dataset inompatible with what is described below')
+        os.remove('./uploads/' + files.filename)
+        return render_template('uploading.html', 
+  textofdisplay=
+  'dataset inompatible with what is described below, please follow follow the guidelines',labeling = 'Red')
+
+
+def storing(dictofdata):
+    for i in dictofdata:
+        print(dictofdata)
+        p = './datasets/' + str(i) + '.csv'
+        if os.path.isfile(p):
+            hld = pd.read_csv('./datasets/' + str(i) + '.csv',index_col=None)
+            print(hld["month"].dtype)
+            hld["month"] = pd.to_datetime(hld["month"]).dt.date
+            hld = hld.append(dictofdata[i]).sort_values(by='month')
+            hld = hld.drop_duplicates().reset_index().drop(columns=['index'])
+            os.remove('./datasets/' + str(i) + '.csv')
+            hld.to_csv('./datasets/' + str(i) + '.csv',index=False)
+            newmodel = auto_arima(hld['quantity'] , start_p = 0, start_q = 0, max_p = 12, max_q = 12, m=12 , max_order = None , start_P = 0, seasonal = True, d = 1 , D = 1, stepwise = True , error_action ='ignore' , random = False)
+            os.remove('./models/' + str(i) + '.pkl')
+            with open('./models/' + str(i) + '.pkl', 'wb') as pkl:
+                pickle.dump(newmodel, pkl)
+            print('model for {} was updated'.format(str(i)))
+        else:
+            dictofdata[i].to_csv('./datasets/' + str(i) + '.csv',index=False)
+            newmodel = auto_arima(hld['quantity'] , start_p = 0, start_q = 0, max_p = 12, max_q = 12, m=12 , max_order = None , start_P = 0, seasonal = True, d = 1 , D = 1, stepwise = True , error_action ='ignore' , random = False)
+            with open('./models/' + str(i) + '.pkl', 'wb') as pkl:
+                pickle.dump(newmodel, pkl)
+            print('model for {} was created'.format(str(i)))
+def dataprocessing(data):
+    dfcopy = data.copy()
+    dfcopy['Date'] = pd.to_datetime(dfcopy['Date'])
+    # seperating the month and the year
+    dfcopy['month'] = pd.DatetimeIndex(dfcopy['Date']).month.astype(str)
+    dfcopy['year'] = pd.DatetimeIndex(dfcopy['Date']).year.astype(str)
+    # specifying the month and the year of each data instance
+    dfcopy['sortingdate'] = pd.to_datetime([f'{y}-{m}-01' for y, m in zip(dfcopy.year, dfcopy.month)])
+    dfcopy['sortingdate'] = pd.to_datetime(dfcopy['sortingdate']).dt.date
+    # revoking the specified day in the data instance so we can gather the data on monthly basis, also revoking the item_category because it already exists in the id_struct
+    dfcopy = dfcopy.drop(columns=['Date','month','year','item_category'])
+    grouped = dfcopy.groupby(['sortingdate','shop_id','item_id'])
+    days = set (dfcopy['sortingdate'])
+    shops = set (dfcopy['shop_id'])
+    items = set (dfcopy['item_id'])
+    data = []
+    for i in days:
+        for j in shops:
+            for k in items:
+                try:
+                    holderbyitem = grouped.get_group((i,j,k)) # getting the specified group
+                except:
+                    continue
+                if not holderbyitem.empty:
+                    quantity = holderbyitem['item_cnt_day'].sum() # summing the quantity across one month
+                    price = holderbyitem['Price'].mean() # averaging the price because the price of a given item can change multiple times within one month
+                    data.append([i,j,k,price,quantity])
+    # creating a dataframe that has instances sorted by month
+    print("<<<<<<<days<<<<<<<<")
+    print(days)
+    print("<<<<<<<shops<<<<<<<<")
+    print(shops)
+    print("<<<<<<<<items<<<<<<<")
+    print(items)
+    print("<<<<<<<redata<<<<<<<<")
+    redata = pd.DataFrame(data, columns=['month','shop_id','item_id','price','quantity'])
+    redata['quantity'] = redata['quantity'].astype('int64')
+    print(redata)
+    print("<<<<<<<<<<<<<<<<<<<<<<")
+    redatacopynmodels = redata.copy()
+    # creating dataframes made of couples of shop_id and item_id and placing them in two ddictionaries; one with data that is monovariate and the other with data that is multivariate
+    datasetswithdelete = dict()
+    structuredgrouped = redatacopynmodels.groupby(['shop_id','item_id'])
+    for i in shops:
+        for j in items:
+            try:
+                # holderbygrp = redatacopynmodels[redatacopynmodels['shop_id'] == i][redatacopynmodels['item_id'] == j]
+                holderbygrp = structuredgrouped.get_group((i,j)).sort_values(by=['month'])
+                1 / holderbygrp.shape[0]
+                print("!!!!!!!!!!!!!!!!!!!!")
+                print(holderbygrp.reset_index())
+                print("!!!!!!!!!!!!!!!!!!!!")
+                datasetswithdelete['shop' + str(i) + 'item' + str(j)] = holderbygrp.reset_index().drop(columns=['index','shop_id','item_id','price'])
+            except:
+                continue
+
+    return datasetswithdelete
+
+if __name__ == '__main__':
+    #app.run(host='localhost', debug=True, port=5000)
+    app.debug = True
+    app.config['SESSION_TYPE'] = 'filesystem'
+    #sess.init_app(app)
+    app.debug = True
+    # Serve the app with gevent
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
---
+++ model.py
@@ -0,0 +1,151 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import datetime
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import pmdarima
+from pmdarima import auto_arima
+from pandas import datetime
+
+
+# putting the data in a pandas dataframe
+df = pd.read_excel('/content/drive/MyDrive/colab_datasets/20xx.xlsx', index_col=None)
+
+
+# making a copy of the dataset to work with so if things didn't go well, we have a copy
+dfcopy = df.copy()
+
+
+#  converting the Date column's type to datetime
+dfcopy['Date'] = pd.to_datetime(dfcopy['Date'])
+# seperating the month and the year
+dfcopy['month'] = pd.DatetimeIndex(dfcopy['Date']).month.astype(str)
+dfcopy['year'] = pd.DatetimeIndex(dfcopy['Date']).year.astype(str)
+# specifying the month and the year of each data instance
+dfcopy['sortingdate'] = pd.to_datetime([f'{y}-{m}-01' for y, m in zip(dfcopy.year, dfcopy.month)])
+dfcopy['sortingdate'] = pd.to_datetime(dfcopy['sortingdate']).dt.date
+# revoking the specified day in the data instance so we can gather the data on monthly basis, also revoking the item_category because it already exists in the id_struct
+dfcopy = dfcopy.drop(columns=['Date','month','year','item_category'])
+
+
+
+# sorting the dataset 
+grouped = dfcopy.groupby(['sortingdate','shop_id','item_id'])
+
+
+
+# extracting the possible days, shops, and items
+days = set (dfcopy['sortingdate'])
+shops = set (dfcopy['shop_id'])
+items = set (dfcopy['item_id'])
+
+
+
+# reshaping the data by gathering them by month,shop_id, and item_id
+data = []
+for i in days:
+  for j in shops:
+    for k in items:
+      try:
+        holderbyitem = grouped.get_group((i,j,k)) # getting the specified group
+      except:
+        continue
+      if not holderbyitem.empty:
+        quantity = holderbyitem['item_cnt_day'].sum() # summing the quantity across one month
+        price = holderbyitem['Price'].mean() # averaging the price because the price of a given item can change multiple times within one month
+        data.append([i,j,k,price,quantity])
+# creating a dataframe that has instances sorted by month
+redata = pd.DataFrame(data, columns=['month','shop_id','item_id' ,'quantity'])
+redata['quantity'] = redata['quantity'].astype('int64')
+# the following line describes the possible ['sortingdate','shop_id','item_id'] groupings given the data that we have. uncommet it and run it to get the output
+# dfcopy[['sortingdate','shop_id','item_id']].drop_duplicates().describe()
+
+
+
+
+# making copies of the dataframs to work with
+redatacopy = redata.copy()
+redatacopyonemodel = redatacopy.copy()
+redatacopynmodels = redatacopy.copy()
+
+
+
+
+
+# in case the data given from the industrial partner are missing for some couples of shop_id and item_id, ASSUMING THAT THE DATA ARE SIMILIAR BETWEEN ALL THE GROUPS FOR THAT COUPLE, we will be replacing them with the data of the corresponding item_id of the shop that sold the most among other shops of that particular product
+def change (j):
+  avshops = list(set(redatacopynmodels[redatacopynmodels['item_id'] == j]['shop_id']))
+  other = list(redatacopynmodels.groupby(['shop_id','item_id']).get_group((k,j)) for k in avshops)
+  shapes = [i.shape[0] for i in other]
+  max = sorted(shapes)[-1]
+  indice = [i for i in range(len(shapes)) if shapes[i] == max][0]
+  return other[indice]
+
+
+
+
+  # creating 457 dataframes made of couples of shop_id and item_id and placing them in two ddictionaries; one with data that is monovariate and the other with data that is multivariate
+datasetswithdelete = {}
+structuredgrouped = redatacopynmodels.groupby(['shop_id','item_id'])
+for i in shops:
+  for j in items:
+    try:
+      holderbygrp = redatacopynmodels[redatacopynmodels['shop_id'] == i][redatacopynmodels['item_id'] == j] #holderbygrp = structuredgrouped.get_group((i,j)).sort_values(by=['month'])
+      1 / holderbygrp.shape[0]
+      datasetswithdelete['shop' + str(i) + 'item' + str(j)] = holderbygrp.reset_index().drop(columns=['index','shop_id','item_id','Price'])
+    except:
+      continue #in case the shop_id , item_id couple doesn't exist, we create for it an empty dataframe
+      #datasetswithdelete['shop' + str(i) + 'item' + str(j)] = change(j).drop(columns=['shop_id' , 'item_id' , 'Price'])
+      #datasetswithdelete['shop' + str(i) + 'item' + str(j)] = pd.DataFrame(columns=['month','quantity'])
+
+
+
+
+'''      
+# creating 475 dataframes made of couples of shop_id and item_id and placing them in two ddictionaries; one with data that is monovariate and the other with data that is multivariate
+datasetswithdelete = {}
+structuredgrouped = redatacopynmodels.groupby(['shop_id','item_id'])
+for i in shops:
+  for j in items:
+    try:
+      holderbygrp = redatacopynmodels[redatacopynmodels['shop_id'] == i][redatacopynmodels['item_id'] == j] #holderbygrp = structuredgrouped.get_group((i,j)).sort_values(by=['month'])
+      datasetswithdelete['shop' + str(i) + 'item' + str(j)] = holderbygrp.reset_index().drop(columns=['index','shop_id','item_id','Price'])
+    except: #in case the shop_id , item_id couple doesn't exist, we create for it an empty dataframe
+      #datasetswithdelete['shop' + str(i) + 'item' + str(j)] = change(j).drop(columns=['shop_id' , 'item_id' , 'Price'])
+      datasetswithdelete['shop' + str(i) + 'item' + str(j)] = pd.DataFrame(columns=['month','quantity'])
+'''
+
+
+
+
+
+
+# appending zeros for the months that did not score any sales of the given item in the given shop
+copyofwithdelete = datasetswithdelete.copy()
+for i in copyofwithdelete:
+  for j in days:
+    try:
+      1 / list(copyofwithdelete[i]['month'] == j).count(True)
+    except:
+      copyofwithdelete[i] = copyofwithdelete[i].append(pd.DataFrame([[j,0]], columns=list(copyofwithdelete[i].columns)))
+  copyofwithdelete[i] = copyofwithdelete[i].reset_index().sort_values(by=['month']).drop(columns=['index']).reset_index().drop(columns=['index'])
+
+
+
+
+models = dict()
+for i in copyofwithdelete:
+  data = pd.Series(copyofwithdelete[i].quantity, index=copyofwithdelete[i].month)
+  print(i)
+  # stepwise_fit = auto_arima(copyofwithdelete['shop7item790380381']['quantity'] , start_p = 0, start_q = 0, max_p = 12, max_q = 12, m=12 , max_order = None , start_P = 0, seasonal = True, d = 1 , D = 1, stepwise = True , error_action ='ignore' , random = False)
+  models[i] = auto_arima(copyofwithdelete[i]['quantity'] , start_p = 0, start_q = 0, max_p = 12, max_q = 12, m=12 , max_order = None , start_P = 0, seasonal = True, d = 1 , D = 1, stepwise = True , error_action ='ignore' , random = False)
+
+
+'''
+forecaster = model.predict(n_periods=20, return_conf_int=False)
+forecaster = forecaster.astype('int64')
+for i in range(len(forecaster)):
+  if forecaster[i] < 0:
+    forecaster[i] = 0
+'''
